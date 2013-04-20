9c5cac13ee65fe3c029ef91920a115c43ee37dbc
Kalen Wessel <kalen.wessel@gmail.com>
2013-04-20 01:25:49 -0700

    Starting from scratch. So far it loads all the members on the page. Loops through and deletes them.

--- fb_delete_allgroupmembers.js
+++ fb_delete_allgroupmembers.js
@@ -1,61 +1,13 @@
-// first go to https://www.facebook.com/groups/XXXX/members/
-// then paste this in the javascript console
+// Javascript code to iterate through all group members and delete them automatically.
+
 deleteAll = [];
 deleteAll.elms = [];
- 
-deleteAll.canClick = function (el) {
-  return (typeof el != 'undefined') && (typeof el.click != 'undefined');
+deleteAll.elms = document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText");
+
+for (var i=1;i<deleteAll.elms.length;i++) {
+	document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText")[i].click()
+	document.getElementsByClassName("_54nc")[1].click()
+	setTimeout(function(){
+        document.getElementsByClassName("_42ft _42fu layerConfirm uiOverlayButton selected _42g- _42gy")[0].click()
+    }, 4000);
 }
- 
-deleteAll.load = function() {
-  deleteAll.elms = document.getElementsByClassName("mhm auxiliaryButton closeButton uiCloseButton");
-  if (deleteAll.elms.length < 5 ) {
-    setTimeout(deleteAll.getMore, 1000);
-  } else {
-    setTimeout(deleteAll.go, 2000);
-  }
-}
- 
-deleteAll.getMore = function() {
-  more = document.getElementsByClassName("pam uiBoxLightblue  uiMorePagerPrimary");
-  if (typeof more != 'undefined' && deleteAll.canClick(more[0])) { 
-    more[0].click();
-    setTimeout(deleteAll.load, 2000);
-  } else {
-    setTimeout(deleteAll.load, 100);
-  }
-}
- 
-deleteAll.go = function() {
-  try {
-    deleteAll.doClick(2);
-  } catch(e) {
-    setTimeout(deleteAll.load, 1000);
-  } finally {
-    setTimeout(deleteAll.load, 500000);
-  }
-}
- 
-deleteAll.doClick = function(i) {
-  deleteAll.elms[i].click();
-  setTimeout(function(){deleteAll.confirm(i + 1)}, 750);
-}
- 
-deleteAll.confirm = function (i) {
-  remove_but = document.getElementsByName("confirm")[0];
- 
-  if (i < deleteAll.elms.length - 5 && deleteAll.canClick(remove_but) && remove_but.value == "Confirm") {
-    remove_but.click();
-    setTimeout(function(){deleteAll.doClick(i)}, 500);
-  } else {
-    setTimeout(deleteAll.getMore, 2000);
-  }
-}
- 
-// deleteAll.fullRestart = function() {
-//   membersLink = document.getElementsByClassName("groupsCleanLink groupsCleanLinksSelected");
-//   if (typeof membersLink != 'undefined' && deleteAll.canClick(membersLink[0])) { membersLink[0].click(); }
-//   setTimeout(deleteAll.go, 5000);
-// }
- 
-deleteAll.load();
