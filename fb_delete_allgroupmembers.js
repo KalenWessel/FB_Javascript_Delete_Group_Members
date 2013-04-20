2948ae7cc70372ff60a84e78ace74546657bf493
Kalen Wessel <kalen.wessel@gmail.com>
2013-04-20 02:17:33 -0700

    Trying to make the function call itself over and over. Chrome console is not playing along.

--- fb_delete_allgroupmembers.js
+++ fb_delete_allgroupmembers.js
@@ -4,10 +4,17 @@ deleteAll = [];
 deleteAll.elms = [];
 deleteAll.elms = document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText");
 
-for (var i=1;i<deleteAll.elms.length;i++) {
-	document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText")[i].click()
+function deleteMembers(userCount){
+	userCount--;
+	if(userCount < 0)
+		return
+	document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText")[userCount].click()
 	document.getElementsByClassName("_54nc")[1].click()
 	setTimeout(function(){
         document.getElementsByClassName("_42ft _42fu layerConfirm uiOverlayButton selected _42g- _42gy")[0].click()
     }, 4000);
+	setTimeout(deleteMembers(userCount),1000);
 }
+
+deleteMembers(deleteAll.elms.length);
+
