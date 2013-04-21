867b92168fe0fbe940f86c1543329117d603fcaa
Kalen Wessel <kalen.wessel@gmail.com>
2013-04-20 22:18:57 -0700

    Recusive issue solved. Had to due with use setInterval. Answer was to use setTimeout and call it correctly. As I write this commit my 4000+ member group is slowly dwiddling away.

--- fb_delete_allgroupmembers.js
+++ fb_delete_allgroupmembers.js
@@ -4,21 +4,21 @@ deleteAll = [];
 deleteAll.elms = [];
 deleteAll.elms = document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText");
 
-function deleteMembers(userCount){
-	dUser = userCount - 1;
-	if(dUser <= 0) 
-		throw new Error("Don't delete admin!");
-
-	document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText")[dUser].click()
-	document.getElementsByClassName("_54nc")[1].click()
+function deleteMembers(dUser){
+	setTimeout(function(){
+		document.getElementsByClassName("uiPopoverButton _p uiButton uiButtonSuppressed uiButtonNoText")[dUser].click()
+	}, 500);
+	setTimeout(function(){
+	    document.getElementsByClassName("_54nc")[1].click()
+	}, 3000);
 	setTimeout(function(){
         document.getElementsByClassName("_42ft _42fu layerConfirm uiOverlayButton selected _42g- _42gy")[0].click()
-    }, 2000);
+    }, 3000);
 	setTimeout(function(){
 		node = document.getElementsByClassName('uiContextualLayerPositioner uiLayer hidden_elem')[0];
 		node.parentNode.removeChild(node);
 	}, 4000);
-	setInterval(function(){deleteMembers(dUser)}, 5000);
+	setTimeout(function() { deleteMembers(dUser - 1); }, 5000)
 }
 
 deleteMembers(deleteAll.elms.length);
